d449af41298467040baf2177c23d2c70fff8f002
alexandre thauvin <alexandre.thauvin@epitech.eu>
2017-11-10 16:30:22 +0100

    sign in/out google

--- android/app/src/main/java/com/prolendar/thauvi_a/prolendar/MainActivity.java
+++ android/app/src/main/java/com/prolendar/thauvi_a/prolendar/MainActivity.java
@@ -1,13 +1,19 @@
 package com.prolendar.thauvi_a.prolendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toSignIn(View view) {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
     }
 }
