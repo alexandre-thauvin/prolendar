ffff35378ccba7c03ee5b94c22750bf9af8d3af6
alexandre thauvin <alexandre.thauvin@epitech.eu>
2017-11-16 23:11:42 +0100

    auth with firebase implement spinner update google-services access to calendar since sigin (for debug)

--- android/app/src/main/java/com/prolendar/thauvi_a/prolendar/FormActivity.java
+++ android/app/src/main/java/com/prolendar/thauvi_a/prolendar/FormActivity.java
@@ -1,6 +1,12 @@
 package com.prolendar.thauvi_a.prolendar;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by thauvi_a on 11/13/17.
@@ -12,4 +18,35 @@ import android.app.Activity;
  */
 
 public class FormActivity extends Activity {
+
+        Spinner spinner;
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.form);
+
+            //Récupération du Spinner déclaré dans le fichier main.xml de res/layout
+            spinner = findViewById(R.id.spinner);
+            //Création d'une liste d'élément à mettre dans le Spinner(pour l'exemple)
+            List<String> exp = new ArrayList<>();
+            exp.add("Coiffeur(se) ");
+            exp.add("Infirmier(e)");
+
+		/*Le Spinner a besoin d'un adapter pour sa presentation alors on lui passe le context(this) et
+                un fichier de presentation par défaut( android.R.layout.simple_spinner_item)
+		Avec la liste des elements (exemple) */
+            ArrayAdapter adapter = new ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item,
+                    exp
+            );
+
+
+               /* On definit une présentation du spinner quand il est déroulé         (android.R.layout.simple_spinner_dropdown_item) */
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            //Enfin on passe l'adapter au Spinner et c'est tout
+            spinner.setAdapter(adapter);
+
+        }
 }
--- android/app/src/main/java/com/prolendar/thauvi_a/prolendar/SignInActivity.java
+++ android/app/src/main/java/com/prolendar/thauvi_a/prolendar/SignInActivity.java
@@ -8,15 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class SignInActivity extends AppCompatActivity implements
@@ -27,29 +32,33 @@ public class SignInActivity extends AppCompatActivity implements
 
     private GoogleSignInClient mGoogleSignInClient;
     private TextView mStatusTextView;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin2);
+        setContentView(R.layout.signin);
         setTitle(R.string.app_name);
         mStatusTextView = findViewById(R.id.email);
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestIdToken(getString(R.string.id_client))
                 .requestId()
                 .requestProfile()
                 .build();
         findViewById(R.id.sign_out_button).setVisibility(View.GONE);
         findViewById(R.id.done).setVisibility(View.GONE);
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     @Override
@@ -64,12 +73,34 @@ public class SignInActivity extends AppCompatActivity implements
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            updateUI(account);
+            firebaseAuthWithGoogle(account);
         } catch (ApiException e) {
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
         }
     }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
@@ -86,7 +117,7 @@ public class SignInActivity extends AppCompatActivity implements
                 });
     }
 
-    private void updateUI(@Nullable GoogleSignInAccount account) {
+    private void updateUI(@Nullable FirebaseUser account) {
         if (account != null) {
             mStatusTextView.setText(account.getEmail());
             findViewById(R.id.sign_in_button).setVisibility(View.GONE);
@@ -118,6 +149,11 @@ public class SignInActivity extends AppCompatActivity implements
 
     public void toCalendar(View view) {
         Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void toForm(View view) {
+        Intent intent = new Intent(this, FormActivity.class);
         startActivity(intent);
     }
 }
